3ebf48d9f9d47ac8beb82402d0664b0fbc88aed9
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2025-11-15 17:44:44 +0530

    removed unused code from Hero Banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -36,74 +36,7 @@ const HeroBanner = () => {
   useEffect(() => {
     fetchPopularMovies();
   }, []);
-  // return (
-  //   <div
-  //     id="default-carousel"
-  //     className="relative w-full"
-  //     data-carousel="slide"
-  //   >
-  //     <div className="relative  h-96 overflow-hidden rounded-lg md:h-96">
-  //       <div key={carouselData[page]?.id} className="" data-carousel-item>
-  //         <img
-  //           className="absolute pt-96 block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-  //           alt="movie"
-  //           src={`https://image.tmdb.org/t/p/original${carouselData[page]?.backdrop_path}`}
-  //         />
-  //         <h1 className="z-2 absolute text-yellow-300 text-2xl font-bold top-4/5 left-1/12">{carouselData[page]?.title}</h1>
-  //       </div>
-  //     </div>
-  //     <button
-  //       onClick={handlePrevious}
-  //       type="button"
-  //       className=" absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-  //       data-carousel-prev
-  //     >
-  //       <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-  //         <svg
-  //           className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-  //           aria-hidden="true"
-  //           xmlns="http://www.w3.org/2000/svg"
-  //           fill="none"
-  //           viewBox="0 0 6 10"
-  //         >
-  //           <path
-  //             stroke="currentColor"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //             strokeWidth="2"
-  //             d="M5 1 1 5l4 4"
-  //           />
-  //         </svg>
-  //         <span className="sr-only">Previous</span>
-  //       </span>
-  //     </button>
-  //     <button
-  //       onClick={handleNext}
-  //       type="button"
-  //       className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-  //       data-carousel-next
-  //     >
-  //       <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-  //         <svg
-  //           className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-  //           aria-hidden="true"
-  //           xmlns="http://www.w3.org/2000/svg"
-  //           fill="none"
-  //           viewBox="0 0 6 10"
-  //         >
-  //           <path
-  //             stroke="currentColor"
-  //             strokeLinecap="round"
-  //             strokeLinejoin="round"
-  //             strokeWidth="2"
-  //             d="m1 9 4-4-4-4"
-  //           />
-  //         </svg>
-  //         <span className="sr-only">Next</span>
-  //       </span>
-  //     </button>
-  //   </div>
-  // );
+
   return (
   <div id="default-carousel" className="relative w-full" data-carousel="slide">
     <div className="relative h-64 sm:h-80 md:h-[500px] overflow-hidden rounded-lg">
